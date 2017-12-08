98a20192ddad301b2ef8cadbe79556c1de820b80
Jon Schneider <jon.schneider@gmail.com>
2017-12-08 08:22:29 -0500

    Add solution for Day 8 Part 1

---
+++ advent-of-code-2017/Day8.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+namespace advent_of_code_2017
+{
+    public class Day8
+    {
+        public void Run()
+        {
+            Dictionary<string, int> registers = new Dictionary<string, int>();
+
+            string[] lines = File.ReadAllLines("day8input.txt");
+            foreach (string line in lines)
+            {
+                string[] tokens = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                if (ShouldPerformOperation(tokens, registers))
+                {
+                    DoOperation(tokens, registers);       
+                }
+            }
+
+            int maxRegisterValue = registers.Values.Max();
+            Console.WriteLine("Part 1 Solution: " + maxRegisterValue);
+        }
+
+        private int GetRegisterValue(string name, Dictionary<string, int> registers)
+        {
+            if (!registers.ContainsKey(name))
+            {
+                registers[name] = 0;
+            }
+            return registers[name];
+        }
+
+        private bool ShouldPerformOperation(string[] tokens, Dictionary<string, int> registers)
+        {
+            int compareRegisterValue = GetRegisterValue(tokens[4], registers);
+            int comparisonValue = Int32.Parse(tokens[6]);
+            bool performOperation;
+            switch (tokens[5])
+            {
+                case ">":
+                    performOperation = compareRegisterValue > comparisonValue;
+                    break;
+                case "<":
+                    performOperation = compareRegisterValue < comparisonValue;
+                    break;
+                case "==":
+                    performOperation = compareRegisterValue == comparisonValue;
+                    break;
+                case "!=":
+                    performOperation = compareRegisterValue != comparisonValue;
+                    break;
+                case ">=":
+                    performOperation = compareRegisterValue >= comparisonValue;
+                    break;
+                case "<=":
+                    performOperation = compareRegisterValue <= comparisonValue;
+                    break;
+                default:
+                    throw new InvalidOperationException("Unrecognized operator token: " + tokens[5]);
+            }
+            return performOperation;
+        }
+
+        private void DoOperation(string[] tokens, Dictionary<string, int> registers)
+        {
+            int modificationValue = Int32.Parse(tokens[2]);
+            int currentRegisterValue = GetRegisterValue(tokens[0], registers);
+            switch (tokens[1])
+            {
+                case "inc":
+                    registers[tokens[0]] = currentRegisterValue + modificationValue;
+                    break;
+                case "dec":
+                    registers[tokens[0]] = currentRegisterValue - modificationValue;
+                    break;
+                default:
+                    throw new InvalidOperationException("Unrecognized modifier token: " + tokens[1]);
+            }
+        }
+    }
+}
--- advent-of-code-2017/Program.cs
+++ advent-of-code-2017/Program.cs
@@ -10,26 +10,17 @@ namespace advent_of_code_2017
     {
         public static void Main(string[] args)
         {
-            Day7 day7 = new Day7();
-            day7.Run();
+            Day8 day = new Day8();
 
-            //Day6 day6 = new Day6();
-            //day6.Run();
+            //Day7 day = new Day7();
+            //Day6 day = new Day6();
+            //Day5 day = new Day5();
+            //Day4 day = new Day4();
+            //Day3 day = new Day3();
+            //Day2 day = new Day2();
+            //Day1 day = new Day1();
 
-            //Day5 day5 = new Day5();
-            //day5.Run();
-
-            //Day4 day4 = new Day4();
-            //day4.Run();
-
-            //Day3 day3 = new Day3();
-            //day3.Run();
-
-            //Day2 day2 = new Day2();
-            //day2.Run();
-
-            //Day1 day1 = new Day1();
-            //day1.Run(args[0]);
+            day.Run();
 
             Console.ReadLine();
         }
