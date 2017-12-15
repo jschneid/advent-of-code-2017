3a7f03241a7918b241b6c7dd8f44ee987f170ed9
Jon Schneider <jon.schneider@gmail.com>
2017-12-15 12:48:27 -0500

    Add solution for Day 15 Part 2

--- advent-of-code-2017/Day15.cs
+++ advent-of-code-2017/Day15.cs
@@ -8,6 +8,12 @@ namespace advent_of_code_2017
         /// </summary>
         public void Run()
         {
+            //Console.Out.WriteLine("Part 1 solution: " + Part1());
+            Console.Out.WriteLine("Part 2 solution: " + Part2());
+        }
+
+        private int Part1()
+        {
             int matches = 0;
             long valueA = 512;
             long valueB = 191;
@@ -21,12 +27,11 @@ namespace advent_of_code_2017
                     matches++;
                 }
             }
-
-            Console.Out.WriteLine("Part 1 solution: " + matches);
+            return matches;
         }
 
         /// <summary>
-        /// Return true if the lowest 16 bits of the two values are the same.
+        /// Returns true if the lowest 16 bits of the two values are the same.
         /// </summary>
         private bool Judge(long valueA, long valueB)
         {
@@ -37,6 +42,46 @@ namespace advent_of_code_2017
         {
             int multiplier = (generator == 'A' ? 16807 : 48271);
             long newValue = (initialValue * multiplier) % 2147483647;
+            return newValue;
+        }
+
+        private int Part2()
+        {
+            int matches = 0;
+            long valueA = 512;
+            long valueB = 191;
+            for (int i = 0; i < 5000000; i++)
+            {
+                valueA = GetNextPart2ValueA(valueA);
+                valueB = GetNextPart2ValueB(valueB);
+
+                if (Judge(valueA, valueB))
+                {
+                    matches++;
+                }
+            }
+            return matches;
+        }
+
+        private long GetNextPart2ValueA(long initialValue)
+        {
+            int multiplier = 16807;
+            long newValue = initialValue;
+            do
+            {
+                newValue = (newValue * multiplier) % 2147483647;
+            } while (newValue % 4 != 0);
+            return newValue;
+        }
+
+        private long GetNextPart2ValueB(long initialValue)
+        {
+            int multiplier = 48271;
+            long newValue = initialValue;
+            do
+            {
+                newValue = (newValue * multiplier) % 2147483647;
+            } while (newValue % 8 != 0);
             return newValue;
         }
     }
